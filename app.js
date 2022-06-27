0cde0de33218622c7bc9fea33c539c6242c7b66a
Nikita <nikitaaggarwal6600@gmail.com>
2022-06-27 23:57:54 +0530

    start added in  json

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express(); // Initialized and server ready
 
 app.use(express.static("public"));
 
-let port = 9898;
+let port = process.env.PORT || 9898;
 let server = app.listen(port, () => {
     console.log("Listening to port " + port);
 })
